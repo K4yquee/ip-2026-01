2988af8fe1628630449163db5d17e9b5d3a67c4f
Kayque <kayquevicente@discente.ufg.br>
2026-04-05 12:07:13 -0300

    Create Q23.go

---
+++ lista02_go/Q23.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var idade int
+
+	fmt.Print("Digite a idade: ")
+	fmt.Scan(&idade)
+
+	if idade < 16 {
+		fmt.Println("Não-eleitor")
+	} else if idade < 18 {
+		fmt.Println("Eleitor facultativo")
+	} else if idade <= 65 {
+		fmt.Println("Eleitor obrigatório")
+	} else {
+		fmt.Println("Eleitor facultativo")
+	}
+}
