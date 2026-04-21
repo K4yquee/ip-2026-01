1cb47f136eed7fd52b9ac966ffc005e22ff5266b
Kayque <kayquevicente@discente.ufg.br>
2026-04-20 22:23:43 -0300

    Create Q4.go

---
+++ lista04-exercícios-recursão/Q4.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	var binario string
+	binario = ""
+
+	fmt.Print("Digite um número decimal: ")
+	fmt.Scan(&n)
+
+	for n > 0 {
+		resto := n % 2
+		binario = fmt.Sprint(resto) + binario
+		n = n / 2
+	}
+
+	fmt.Println("Binário:", binario)
+}
