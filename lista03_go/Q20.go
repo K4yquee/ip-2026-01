35e69c76851e2f700ae94a8cd48bc72195220809
Kayque <kayquevicente@discente.ufg.br>
2026-04-20 22:10:39 -0300

    Create Q20.go

---
+++ lista03_go/Q20.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 0; i < 6; i++ {
+		for j := 0; j < 6; j++ {
+
+			if i > j {
+				fmt.Printf("(%d,%d) ", i, j)
+			}
+		}
+		fmt.Println()
+	}
+}
