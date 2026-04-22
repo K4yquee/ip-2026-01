ac353295fe6ed5f416ea5904a1c7000580dcbd6c
Kayque <kayquevicente@discente.ufg.br>
2026-04-22 08:00:45 -0300

    Implement recursive sum function for float64 slice

--- lista04-exercícios-recursão/Q2.go
+++ lista04-exercícios-recursão/Q2.go
@@ -2,13 +2,17 @@ package main
 
 import "fmt"
 
+func somaRecursiva(valores []float64, indice int) float64 {
+	if indice == len(valores) {
+		return 0
+	}
+	return valores[indice] + somaRecursiva(valores, indice+1)
+}
+
 func main() {
 	valores := []float64{2.5, 3.0, 4.5, 1.0}
-	soma := 0.0
 
-	for i := 0; i < len(valores); i++ {
-		soma += valores[i]
-	}
+	soma := somaRecursiva(valores, 0)
 
 	fmt.Println("Soma:", soma)
 }
