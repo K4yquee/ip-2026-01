859a1b35c407dbfd54f928b5555fc0c3e8e39c18
Kayque <kayquevicente@discente.ufg.br>
2026-04-22 07:59:07 -0300

    Implement recursive power function for calculation

--- lista04-exercícios-recursão/Q1.go
+++ lista04-exercícios-recursão/Q1.go
@@ -2,9 +2,15 @@ package main
 
 import "fmt"
 
+func potencia(x, n int) int {
+	if n == 0 {
+		return 1
+	}
+	return x * potencia(x, n-1)
+}
+
 func main() {
 	var x, n int
-	resultado := 1
 
 	fmt.Print("Digite o valor de x: ")
 	fmt.Scan(&x)
@@ -12,9 +18,7 @@ func main() {
 	fmt.Print("Digite o valor de n: ")
 	fmt.Scan(&n)
 
-	for i := 0; i < n; i++ {
-		resultado *= x
-	}
+	resultado := potencia(x, n)
 
 	fmt.Println("Resultado:", resultado)
 }
